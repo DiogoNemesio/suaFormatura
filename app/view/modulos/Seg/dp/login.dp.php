b71b74f01b8e3100debdd8444337f00eb1c2ebc9
jalonVitor <jalon@zage.com.br>
2015-07-28 11:15:58 -0300

    -Recuperacao de senha -Confirmacao de email

---
+++ app/view/modulos/Seg/dp/login.dp.php
@@ -0,0 +1,147 @@
+<?php
+#################################################################################
+## Includes
+#################################################################################
+if (defined('DOC_ROOT')) {
+	include_once(DOC_ROOT . 'includeNoAuth.php');
+}else{
+ 	include_once('../includeNoAuth.php');
+}
+
+use \Zend\Mail;
+use \Zend\Mail\Message;
+use \Zend\Mime\Message as MimeMessage;
+use \Zend\Mime\Part as MimePart;
+Use \Zend\Mime;
+
+#################################################################################
+## Resgata os parâmetros passados pelo formulario
+#################################################################################
+if (isset($_POST['email'])) 			$email				= \Zage\App\Util::antiInjection($_POST['email']);
+
+#################################################################################
+## Limpar a variável de erro
+#################################################################################
+$err	= false;
+
+#################################################################################
+## Fazer validação dos campos
+#################################################################################
+/** Email **/
+if (!isset($email) || (empty($email))) {
+	die ('1'.\Zage\App\Util::encodeUrl('||'.htmlentities($tr->trans("Campo EMAIL é obrigatório !!!"))));
+	$err	= 1;
+}
+
+$valEmail	= new \Zage\App\Validador\Email();
+if ($valEmail->isValid($email) == false){
+	die ('1'.\Zage\App\Util::encodeUrl('||'.htmlentities($tr->trans("EMAIL inválido !!!"))));
+	$err	= 1;
+}
+
+$oEmail	= $em->getRepository('Entidades\ZgsegUsuario')->findOneBy(array('usuario' => $email));
+
+if (($oEmail != null) && ($oEmail->getUsuario() == $email)){
+	$codUsuario = $oEmail;
+	$log->debug('email existe');
+}else{
+	$log->debug('email nao existe');
+	die ('1'.\Zage\App\Util::encodeUrl('||'.htmlentities($tr->trans("Email não existe!"))));
+	$err 	= 1;
+}
+
+if ($err != null) {
+	echo '1'.\Zage\App\Util::encodeUrl('||'.htmlentities($err));
+ 	exit;
+}
+
+#################################################################################
+## Salvar no banco
+#################################################################################
+try {
+	
+	$oRecSenha	= $em->getRepository('Entidades\ZgsegUsuarioRecSenha')->findOneBy(array('codUsuario' => $codUsuario, 'codStatus' => 'A'));
+	if (!$oRecSenha){
+		$oRecSenha	= new \Entidades\ZgsegUsuarioRecSenha();
+	}else{
+		die ('1'.\Zage\App\Util::encodeUrl('||'.htmlentities($tr->trans("Existe confirmação de email pendende!"))));
+		exit;
+	}
+	
+ 	if (!$oRecSenha) $oRecSenha	= new \Entidades\ZgsegUsuarioRecSenha();
+ 	
+ 	$oStatus	= $em->getRepository('Entidades\ZgsegHistEmailStatus')->findOneBy(array('codigo' => 'A'));
+ 	
+ 	$oRecSenha->setCodUsuario($codUsuario);
+ 	$oRecSenha->setDataSolicitacao(new \DateTime("now"));
+ 	$oRecSenha->setIpSolicitacao(\Zage\App\Util::getIPUsuario());
+ 	$oRecSenha->setSenhaAlteracao(\Zage\Seg\Perfil::_geraSenha());
+ 	$oRecSenha->setCodStatus($oStatus);
+ 	
+ 	$em->persist($oRecSenha);
+ 	$em->flush();
+ 	$em->detach($oRecSenha);
+ 	
+ 	//$texto 		= 'O email: '.$email.' foi confirmado com sucesso, uma nova confirmação foi enviada para o novo email.';
+ 	$cid 		= \Zage\App\Util::encodeUrl('_cdu01='.$oRecSenha->getCodigo().'&_cdu02='.$oRecSenha->getSenhaAlteracao().'&_cdu03='.$oEmail->getUsuario().'&_cdu04='.$oRecSenha->getCodUsuario()->getCodigo());
+ 	$textoEmail	= "Seu email foi confirmado, mas ainda precisa alterar a senha. Para isso, clique no link abaixo:";
+
+ 	#################################################################################
+ 	## Carregando o template html do email
+ 	#################################################################################
+ 	$tpl		= new \Zage\App\Template();
+ 	
+ 	$tpl->load(MOD_PATH . "/App/html/perfilConfirmEmail.html");
+ 	$assunto			= "Alteração de email";
+ 	$nome				= $oRecSenha->getCodUsuario()->getNome();
+ 	$confirmUrl			= ROOT_URL . "/Seg/u04.php?cid=".$cid;
+ 	 	
+ 	#################################################################################
+ 	## Define os valores das variáveis
+ 	#################################################################################
+ 	$tpl->set('ID'					,$id);
+ 	$tpl->set('CONFIRM_URL'			,$confirmUrl);
+ 	$tpl->set('ASSUNTO'				,$assunto);
+ 	$tpl->set('TEXTO'				,$textoEmail);
+ 	$tpl->set('NOME'				,$nome);
+ 	
+ 	#################################################################################
+ 	## Criar os objeto do email ,transporte e validador
+ 	#################################################################################
+ 	$mail 			= \Zage\App\Mail::getMail();
+ 	$transport 		= \Zage\App\Mail::getTransport();
+ 	$validator 		= new \Zend\Validator\EmailAddress();
+ 	$htmlMail 		= new MimePart($tpl->getHtml());
+ 	$htmlMail->type = "text/html";
+ 	$body 			= new MimeMessage();
+ 	
+ 	#################################################################################
+ 	## Definir o conteúdo do e-mail
+ 	#################################################################################
+ 	$body->setParts(array($htmlMail));
+ 	$mail->setBody($body);
+ 	$mail->setSubject("<ZageMail> ".$assunto);
+ 	
+ 	#################################################################################
+ 	## Definir os destinatários
+ 	#################################################################################
+ 	$mail->addTo($email);
+ 	$tpl = null;
+ 	#################################################################################
+ 	## Salvar as informações e enviar o e-mail
+ 	#################################################################################
+ 	try {
+ 		$transport->send($mail);
+ 	} catch (Exception $e) {
+ 		$log->debug("Erro ao enviar o e-mail:". $e->getTraceAsString());
+ 		throw new \Exception("Erro ao enviar o email, a mensagem foi para o log dos administradores, entre em contato para mais detalhes !!!");
+	}
+	
+} catch (\Exception $e) {
+ 	$system->criaAviso(\Zage\App\Aviso\Tipo::ERRO,$e->getMessage());
+ 	echo '1'.\Zage\App\Util::encodeUrl('||'.htmlentities($e->getMessage()));
+ 	exit;
+}
+ 
+die ('1'.\Zage\App\Util::encodeUrl('||'.htmlentities($tr->trans("Email enviado com sucesso!"))));
+echo '0'.\Zage\App\Util::encodeUrl('|'.$oRecSenha->getCodigo());
---
+++ app/view/modulos/Seg/php/u04.php
@@ -0,0 +1,64 @@
+<?php
+#################################################################################
+## Includes
+#################################################################################
+if (defined('DOC_ROOT')) {
+	include_once(DOC_ROOT . 'includeNoAuth.php');
+}else{
+	include_once('includeNoAuth.php');
+}
+
+#################################################################################
+## Resgata a variável CID que está criptografada
+#################################################################################
+if (isset($_GET['cid'])) $cid = \Zage\App\Util::antiInjection($_GET["cid"]);
+if (!isset($cid))		\Zage\App\Erro::externalHalt('Script só pode ser usado por pessoas autorizadas');
+
+#################################################################################
+## Descompacta o ID
+#################################################################################
+\Zage\App\Util::descompactaId($cid);
+
+if (!isset($_cdu01))	\Zage\App\Erro::externalHalt('Script só pode ser usado por pessoas autorizadas, COD_ERRO: 01');
+if (!isset($_cdu02))	\Zage\App\Erro::externalHalt('Script só pode ser usado por pessoas autorizadas, COD_ERRO: 02');
+if (!isset($_cdu03))	\Zage\App\Erro::externalHalt('Script só pode ser usado por pessoas autorizadas, COD_ERRO: 03');
+if (!isset($_cdu04))	\Zage\App\Erro::externalHalt('Script só pode ser usado por pessoas autorizadas, COD_ERRO: 04');
+
+#################################################################################
+## Ajusta os nomes das variáveis
+#################################################################################
+$codRecSenha	= $_cdu01;
+$senhaAlteracao	= $_cdu02;
+$email			= $_cdu03;
+$codUsuario		= $_cdu04;
+
+#################################################################################
+## Verificar se os usuário já existe e se já está ativo
+#################################################################################
+$oRecSenha	= $em->getRepository('Entidades\ZgsegUsuarioRecSenha')->findOneBy(array('codigo' => $codRecSenha));
+if (!$oRecSenha) 											\Zage\App\Erro::externalHalt('Recuperação de senha não está mais disponível, COD_ERRO: 05');
+if ($oRecSenha->getCodStatus()->getCodigo() != "A")			\Zage\App\Erro::externalHalt('Recuperação de senha não está mais disponível, COD_ERRO: 06');
+if ($oRecSenha->getSenhaAlteracao() != $senhaAlteracao)		\Zage\App\Erro::externalHalt('Senha não correspondente, COD_ERRO: 07');
+
+$oEmail	= $em->getRepository('Entidades\ZgsegUsuario')->findOneBy(array('codigo' => $codUsuario));
+if (!$oEmail) 												\Zage\App\Erro::externalHalt('Usuario não existe, COD_ERRO: 08');
+#################################################################################
+## Carregando o template html
+#################################################################################
+$tpl	= new \Zage\App\Template();
+$tpl->load(\Zage\App\Util::getCaminhoCorrespondente(__FILE__, \Zage\App\ZWS::EXT_HTML));
+
+#################################################################################
+## Define os valores das variáveis
+#################################################################################
+$tpl->set('URL_FORM'			,$_SERVER['SCRIPT_NAME']);
+$tpl->set('CD01'				,$_cdu01);
+$tpl->set('CD02'				,$_cdu02);
+$tpl->set('CD03'				,$_cdu03);
+$tpl->set('CD04'				,$_cdu04);
+$tpl->set('DP'					,\Zage\App\Util::getCaminhoCorrespondente(__FILE__,\Zage\App\ZWS::EXT_DP,\Zage\App\ZWS::CAMINHO_RELATIVO));
+
+#################################################################################
+## Por fim exibir a página HTML
+#################################################################################
+$tpl->show();
