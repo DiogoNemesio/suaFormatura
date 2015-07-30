47ff17e65286c40a066b7f3fa6555530bb1d1f3a
DiogoNemesio <diogo@zage.com.br>
2015-07-29 23:41:03 -0300

    - Atualização de entidades

---
+++ app/classes/Entidades/ZgfmtRifaVendaSequencial.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Entidades;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ZgfmtRifaVendaSequencial
+ *
+ * @ORM\Table(name="ZGFMT_RIFA_VENDA_SEQUENCIAL")
+ * @ORM\Entity
+ */
+class ZgfmtRifaVendaSequencial
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="CODIGO", type="bigint", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $codigo;
+
+
+    /**
+     * Get codigo
+     *
+     * @return integer 
+     */
+    public function getCodigo()
+    {
+        return $this->codigo;
+    }
+}
