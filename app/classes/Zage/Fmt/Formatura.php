8d645e2b8192ee104969a05acb22a73ef4a4b832
DanielCassela <daniel@zage.com.br>
2015-08-14 21:31:00 -0300

    - Tela de geração de contas

--- app/classes/Zage/Fmt/Formatura.php
+++ app/classes/Zage/Fmt/Formatura.php
@@ -84,4 +84,63 @@ class Formatura {
 			
 	}
 	
+	/**
+	 * Resgata o número de formandos ativos de uma organização
+	 * @param number $codOrganizacao
+	 */
+	public static function getNumFormandosAtivos($codOrganizacao) {
+		global $em,$system;
+	
+		$qb 	= $em->createQueryBuilder();
+			
+		try {
+			$qb->select('count(u.codigo)')
+			->from('\Entidades\ZgsegUsuario','u')
+			->leftJoin('\Entidades\ZgsegUsuarioOrganizacao'	,'uo',	\Doctrine\ORM\Query\Expr\Join::WITH, 'u.codigo 	= uo.codUsuario')
+			->leftJoin('\Entidades\ZgadmOrganizacao'		,'o',	\Doctrine\ORM\Query\Expr\Join::WITH, 'o.codigo 	= uo.codOrganizacao')
+			->where($qb->expr()->andX(
+					$qb->expr()->eq('uo.codOrganizacao'			, ':codOrganizacao'),
+					$qb->expr()->in('uo.codPerfil'				, ':perfil'),
+					$qb->expr()->in('uo.codStatus'				, ':status')
+			))
+			->setParameter('codOrganizacao'	, $codOrganizacao)
+			->setParameter('status'			, array("A"))
+			->setParameter('perfil'			, array(4,5));
+	
+			$query 		= $qb->getQuery();
+			return($query->getSingleScalarResult());
+		} catch (\Exception $e) {
+			\Zage\App\Erro::halt($e->getMessage());
+		}
+			
+	}
+	
+	
+	/**
+	 *
+	 * Calcula o valor total da Formatura
+	 */
+	public static function getValorTotal ($codFormatura) {
+		global $em,$system;
+	
+		$qb 	= $em->createQueryBuilder();
+		try {
+			$qb->select('sum(cp.valor + cp.valorJuros + cp.valorMora - (cp.valorDesconto + cp.valorCancelado))')
+			->from('\Entidades\ZgfinContaPagar','cp')
+			->where($qb->expr()->andX(
+				$qb->expr()->eq('cp.codOrganizacao'	, ':codOrganizacao'),
+				$qb->expr()->notIn('cp.codStatus'	, ':codStatus')
+			))
+			->orderBy('cp.codStatus','ASC')
+			->addOrderBy('cp.dataEmissao'	,'DESC')
+			->setParameter('codStatus'		, array('C','S'))
+			->setParameter('codOrganizacao'	, $system->getCodOrganizacao());
+				
+			$query 		= $qb->getQuery();
+			return($query->getSingleScalarResult());
+		} catch (\Exception $e) {
+			\Zage\App\Erro::halt($e->getMessage());
+		}
+	}
+	
 }
--- app/view/modulos/Fin/php/geraContaMassa.php
+++ app/view/modulos/Fin/php/geraContaMassa.php
@@ -56,14 +56,36 @@ $aSelFormandos		= explode(",",$aSelFormandos);
 #################################################################################
 ## Resgata as informações do banco
 #################################################################################
-$formandos		= $em->getRepository('Entidades\ZgsegUsuario')->findBy(array('codigo' => $aSelFormandos));
+try {
+
+	$formandos	= $em->getRepository('Entidades\ZgsegUsuario')->findBy(array('codigo' => $aSelFormandos));
+	$oOrgFmt	= $em->getRepository('Entidades\ZgfmtOrganizacaoFormatura')->findOneBy(array('codOrganizacao' => $system->getCodOrganizacao()));
+//	$contrato	= $em->getRepository('Entidades\ZgadmContrato')->findOneBy(array('codOrganizacao' => $system->getCodOrganizacao()));
+
+} catch (\Exception $e) {
+	\Zage\App\Erro::halt($e->getMessage());
+}
 
 if (sizeof($formandos) == 0) 	\Zage\App\Erro::halt($tr->trans('Formando[s] não encontrado !!!'));
 
 #################################################################################
-## Inicia o html da tabela
+## Calcular as informações
 #################################################################################
-$htmlTab	= '';
+$totalformandos			= \Zage\Fmt\Formatura::getNumFormandosAtivos($system->getCodOrganizacao());
+$numFormandos			= sizeof($formandos);
+$dataConclusao			= $oOrgFmt->getDataConclusao();
+if (!$dataConclusao)		\Zage\App\Erro::halt($tr->trans('Data de conclusão da formatura não informada!!!'));
+
+$hoje					= new DateTime('now');
+$interval				= $dataConclusao->diff($hoje);
+$numMesesConc			= $interval->format('%m');
+$diaVencimento			= $oOrgFmt->getDiaVencimento();
+$valorPorFormando		= \Zage\App\Util::formataDinheiro($oOrgFmt->getValorPorFormando());
+$valorPorBoleto			= \Zage\App\Util::formataDinheiro($oOrgFmt->getValorPorBoleto());
+$taxaPorFormando		= \Zage\App\Util::formataDinheiro(\Zage\Adm\Contrato::getValorLicenca($system->getCodOrganizacao()));
+$valorTotalFormatura	= \Zage\App\Util::formataDinheiro(\Zage\Fmt\Formatura::getValorTotal($system->getCodOrganizacao()));
+$totalPorFormando		= \Zage\App\Util::formataDinheiro(($valorTotalFormatura / $totalformandos));
+$valorMensalidade		= \Zage\App\Util::formataDinheiro((($valorTotalFormatura / $numFormandos) / $numMesesConc));
 
 #################################################################################
 ## Faz o loop nas parcelas para montar a tabela
@@ -71,6 +93,47 @@ $htmlTab	= '';
 for ($i = 0 ;$i < sizeof($formandos); $i++) {
 
 }
+
+if ($valorPorFormando	< 0) 	$valorPorFormando	= 0;
+if ($valorPorBoleto		< 0)	$valorPorBoleto		= 0;
+if ($taxaPorFormando	< 0)	$taxaPorFormando	= 0;
+if (!$diaVencimento)			$diaVencimento		= 5;
+
+#################################################################################
+## Montar o select do dia de vencimento
+#################################################################################
+$oDiaVenc	= "";
+for ($i = 1; $i <= 31; $i++) {
+	$selected = ($i == $diaVencimento) ? " selected " : "";
+	$dia	= str_pad($i, 2, "0",STR_PAD_LEFT);
+	if ($i > 28) {
+		$dia	.= " * (ou o último dia do mês)";
+	}
+
+	$oDiaVenc	.= "<option value='".$i."' $selected>".$dia."</option>";
+}
+
+#################################################################################
+## Montar o select do número de meses
+#################################################################################
+$oNumMeses	= "";
+for ($i = 1; $i <= $numMesesConc; $i++) {
+	if ($i == 1) {
+		$selected 	= null;
+		$meses		= $i . " Mês";
+	}else{
+		$meses		= $i . " Meses";
+		if ($i == $numMesesConc) {
+			$meses 		.= " (todos)";
+			$selected 	= "selected";
+		}else{
+			$selected 	= null;
+		}
+		
+	}
+	$oNumMeses	.= "<option value='".$i."' $selected>".$meses."</option>";
+}
+
 
 #################################################################################
 ## Urls
@@ -86,10 +149,21 @@ $tpl->load(\Zage\App\Util::getCaminhoCorrespondente(__FILE__, \Zage\App\ZWS::EXT
 #################################################################################
 ## Define os valores das variáveis
 #################################################################################
-$tpl->set('ID'					,$id);
-$tpl->set('TITULO'				,'Geração de Contas');
-$tpl->set('URL_VOLTAR'			,$urlVoltar);
-$tpl->set('DP_MODAL'			,\Zage\App\Util::getCaminhoCorrespondente(__FILE__,\Zage\App\ZWS::EXT_DP,\Zage\App\ZWS::CAMINHO_RELATIVO));
+$tpl->set('ID'						,$id);
+$tpl->set('TITULO'					,'Geração de Contas');
+$tpl->set('URL_VOLTAR'				,$urlVoltar);
+$tpl->set('DIAS_VENC'				,$oDiaVenc);
+$tpl->set('NUMERO_MESES'			,$oNumMeses);
+$tpl->set('NUM_FORMANDOS'			,$numFormandos);
+$tpl->set('TOTAL_FORMANDOS'			,$totalformandos);
+$tpl->set('NUM_MESES_CONCLUSAO'		,$numMesesConc);
+$tpl->set('DATA_CONCLUSAO'			,$dataConclusao->format($system->config["data"]["dateFormat"]));
+$tpl->set('VALOR_TOTAL_FORM_FMT'	,\Zage\App\Util::to_money($valorTotalFormatura));
+$tpl->set('VALOR_TOTAL_FORMATURA'	,$valorTotalFormatura);
+$tpl->set('TOTAL_POR_FORMANDO'		,$totalPorFormando);
+$tpl->set('TOTAL_POR_FORMANDO_FMT'	,\Zage\App\Util::to_money($totalPorFormando));
+$tpl->set('VALOR_FORMANDO'			,$valorMensalidade);
+$tpl->set('DP_MODAL'				,\Zage\App\Util::getCaminhoCorrespondente(__FILE__,\Zage\App\ZWS::EXT_DP,\Zage\App\ZWS::CAMINHO_RELATIVO));
 
 #################################################################################
 ## Por fim exibir a página HTML
