c9032616fcb1e4d9fe2332f8224678735820ee68
DiogoNemesio <diogo@zage.com.br>
2015-11-14 03:18:20 -0300

    - ATUALIZAÇÃO DE ENTIDADES

---
+++ app/classes/Entidades/ZgfmtConviteExtraVendaItem.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace Entidades;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ZgfmtConviteExtraVendaItem
+ *
+ * @ORM\Table(name="ZGFMT_CONVITE_EXTRA_VENDA_ITEM", indexes={@ORM\Index(name="fk_ZGFMT_CONVITE_EXTRA_ITEM_1_idx", columns={"COD_VENDA"}), @ORM\Index(name="fk_ZGFMT_CONVITE_EXTRA_ITEM_2_idx", columns={"COD_CONVITE_CONF"})})
+ * @ORM\Entity
+ */
+class ZgfmtConviteExtraVendaItem
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="CODIGO", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $codigo;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="QUANTIDADE", type="integer", nullable=false)
+     */
+    private $quantidade;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="VALOR_UNITARIO", type="float", precision=10, scale=0, nullable=false)
+     */
+    private $valorUnitario;
+
+    /**
+     * @var \Entidades\ZgfmtConviteExtraVenda
+     *
+     * @ORM\ManyToOne(targetEntity="Entidades\ZgfmtConviteExtraVenda")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="COD_VENDA", referencedColumnName="CODIGO")
+     * })
+     */
+    private $codVenda;
+
+    /**
+     * @var \Entidades\ZgfmtConviteExtraEventoConf
+     *
+     * @ORM\ManyToOne(targetEntity="Entidades\ZgfmtConviteExtraEventoConf")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="COD_CONVITE_CONF", referencedColumnName="CODIGO")
+     * })
+     */
+    private $codConviteConf;
+
+
+    /**
+     * Get codigo
+     *
+     * @return integer 
+     */
+    public function getCodigo()
+    {
+        return $this->codigo;
+    }
+
+    /**
+     * Set quantidade
+     *
+     * @param integer $quantidade
+     * @return ZgfmtConviteExtraVendaItem
+     */
+    public function setQuantidade($quantidade)
+    {
+        $this->quantidade = $quantidade;
+
+        return $this;
+    }
+
+    /**
+     * Get quantidade
+     *
+     * @return integer 
+     */
+    public function getQuantidade()
+    {
+        return $this->quantidade;
+    }
+
+    /**
+     * Set valorUnitario
+     *
+     * @param float $valorUnitario
+     * @return ZgfmtConviteExtraVendaItem
+     */
+    public function setValorUnitario($valorUnitario)
+    {
+        $this->valorUnitario = $valorUnitario;
+
+        return $this;
+    }
+
+    /**
+     * Get valorUnitario
+     *
+     * @return float 
+     */
+    public function getValorUnitario()
+    {
+        return $this->valorUnitario;
+    }
+
+    /**
+     * Set codVenda
+     *
+     * @param \Entidades\ZgfmtConviteExtraVenda $codVenda
+     * @return ZgfmtConviteExtraVendaItem
+     */
+    public function setCodVenda(\Entidades\ZgfmtConviteExtraVenda $codVenda = null)
+    {
+        $this->codVenda = $codVenda;
+
+        return $this;
+    }
+
+    /**
+     * Get codVenda
+     *
+     * @return \Entidades\ZgfmtConviteExtraVenda 
+     */
+    public function getCodVenda()
+    {
+        return $this->codVenda;
+    }
+
+    /**
+     * Set codConviteConf
+     *
+     * @param \Entidades\ZgfmtConviteExtraEventoConf $codConviteConf
+     * @return ZgfmtConviteExtraVendaItem
+     */
+    public function setCodConviteConf(\Entidades\ZgfmtConviteExtraEventoConf $codConviteConf = null)
+    {
+        $this->codConviteConf = $codConviteConf;
+
+        return $this;
+    }
+
+    /**
+     * Get codConviteConf
+     *
+     * @return \Entidades\ZgfmtConviteExtraEventoConf 
+     */
+    public function getCodConviteConf()
+    {
+        return $this->codConviteConf;
+    }
+}
