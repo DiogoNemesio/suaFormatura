51a0ab87979affc27c4a7ae56f4b30737f775766
DanielCassela <daniel@zage.com.br>
2016-02-12 21:55:14 -0300

    - Andamento da tela de contrato de fornecedor

--- app/view/modulos/Fmt/php/contratoFornecCad.php
+++ app/view/modulos/Fmt/php/contratoFornecCad.php
@@ -48,10 +48,16 @@ try {
 	if (!$oItemOrc)	die("Falta de parâmetros 2");
 	$oItemContrato	= $em->getRepository('Entidades\ZgfmtItemOrcContrato')->findOneBy(array('codOrganizacao' => $system->getCodOrganizacao(), 'codItemOrcamento' => $codItem));
 	if ($oItemContrato)	{
-		$oItens			= $em->getRepository('Entidades\ZgfmtItemOrcContratoFornec')->findBy(array('codItemContrato' => $oItemContrato->getCodigo()));
+		$oItens		= $em->getRepository('Entidades\ZgfmtItemOrcContratoFornec')->findBy(array('codItemContrato' => $oItemContrato->getCodigo()));
 	}else{
-		$oItens			= null;
+		$oItens		= null;
 	}
+	
+	
+	#################################################################################
+	## Resgata os fornecedores
+	#################################################################################
+	$aPessoas			= \Zage\Fin\Pessoa::busca($system->getCodOrganizacao(),null,false,true,false);
 	
 } catch (\Exception $e) {
 	\Zage\App\Erro::halt($e->getMessage());
@@ -64,10 +70,26 @@ $tabItens		= "";
 $qtdeContratada	= 0;
 $qtdeEvento		= $oItemOrc->getQuantidade();
 for ($i = 0; $i < sizeof($oItens); $i++) {
-	$tabItens	.= "";
-	$qtdeContratada	+= $oItens[$i]->getQuantidade();
+	$qtde			= $oItens[$i]->getQuantidade();
+	$codFornecedor	= $oItens[$i]->getCodPessoa();
+	$fornecedor		= ($oItens[$i]->getCodPessoa()) ? $oItens[$i]->getCodPessoa()->getNome() : null;
+	$data			= ($oItens[$i]->getDataOperacao()) ? $oItens[$i]->getDataOperacao()->format($system->config["data"]["dateFormat"]) : null;
+	$codItem		= $oItens[$i]->getCodigo();
+	$qtdeContratada	+= $qtde;
+	$tabItens		.= '<tr><td class="center">'.$qtde.'<input type="hidden" name="aQtde[]" value="'.$qtde.'"></td><td class="">'.$fornecedor.'<input type="hidden" name="aCodPessoa[]" value="'.$codFornecedor.'"></td><td class="center">'.$data.'<input type="hidden" name="aData[]" value="'.$data.'"></td><td class="center"><span class="center" zgdelete onclick="delRowConForCad($(this));"><i class="fa fa-trash bigger-150 red"></i></span><input type="hidden" name="codItem[]" value="'.$codItem.'"></td></tr>';
 }
+$falta			= $qtdeEvento - $qtdeContratada;
 
+
+
+#################################################################################
+## Select dos Fornecedores
+#################################################################################
+try {
+	$oPessoas	= $system->geraHtmlCombo($aPessoas,	'CODIGO', 'NOME',	null, null);
+} catch (\Exception $e) {
+	\Zage\App\Erro::halt($e->getMessage(),__FILE__,__LINE__);
+}
 
 #################################################################################
 ## Título da página
@@ -88,11 +110,14 @@ $tpl->set('URL_FORM'				,$_SERVER['SCRIPT_NAME']);
 $tpl->set('ID'						,$id);
 $tpl->set('TITULO'					,$titulo);
 $tpl->set('TAB_ITENS'				,$tabItens);
+$tpl->set('FORNECEDORES'			,$oPessoas);
 $tpl->set('QTDE_CONTRATADA'			,$qtdeContratada);
 $tpl->set('QTDE_EVENTO'				,$qtdeEvento);
+$tpl->set('QTDE_FALTA'				,$falta);
 $tpl->set('DP'						,\Zage\App\Util::getCaminhoCorrespondente(__FILE__,\Zage\App\ZWS::EXT_DP,\Zage\App\ZWS::CAMINHO_RELATIVO));
 $tpl->set('IC'						,$_icone_);
 $tpl->set('COD_MENU'				,$_codMenu_);
+$tpl->set('FORMATO_DATA'			,$system->config["data"]["jsDateFormat"]);
 
 #################################################################################
 ## Por fim exibir a página HTML
